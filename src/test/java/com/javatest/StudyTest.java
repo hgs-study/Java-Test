0c7568996837f2eb05b2a0de5a04da4b3eff3236
hgs-study <hgstudy_@naver.com>
2021-06-04 22:10:23 +0900

    assertThat(assertj 사용)

--- src/test/java/com/javatest/StudyTest.java
+++ src/test/java/com/javatest/StudyTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.*;
 import java.time.Duration;
 import java.util.function.Supplier;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 //언더스코어 공백 치환
@@ -15,24 +16,10 @@ class StudyTest {
     @DisplayName("스터디 만들기")
     void create_new_study(){
 
-        //assertTimeout 은 Duration를 인자로 받고 10초 안에 스터디가 생성되어야 성공
-        assertTimeout(Duration.ofSeconds(10), () -> new Study(10));
 
-
-        // 코드블록({ }) 안에 로직이 300밀리 세컨드로 설정되어있는데 이 300밀리 세컨드를 다 기다리고 테스트가 완료된다.
-        assertTimeout(Duration.ofMillis(100), () -> {
-            new Study(10);
-            Thread.sleep(300);
-        });
-
-        // 코드블록({ }) 안에 로직이 300밀리 세컨드로 설정되어있는데  이 테스트는 100밀리 세컨드가 넘으면 즉시 완료된다
-        // 문제점 : ThreadLocal을 사용하는 로직이 있으면 예상치 못한 에러를 발생시킬 수 있다.
-        // ThreadLocal : Spring Transaction은 ThreadLocal을 사용하는데, 다른 쓰레드에선 공유가 안 된다.
-        // -> 스프링 트랜잭션 설정이 제대로 테스트 안 될 수가 있다. (롤백이 안 되고 디비에 반영될 수 있다.)
-        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
-            new Study(10);
-            Thread.sleep(300);
-        });
+        // assertThat(assertj 사용)
+        Study actual = new Study(10);
+        assertThat(actual.getLimit()).isGreaterThan(0);
 
 
     }
