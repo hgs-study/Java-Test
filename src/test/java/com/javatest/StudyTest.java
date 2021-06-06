da2cd6c05ffad1d0f3bf9165e55f748541f1b760
hgs-study <hgstudy_@naver.com>
2021-06-06 15:55:25 +0900

    assumeTrue / assumingThat

--- src/test/java/com/javatest/StudyTest.java
+++ src/test/java/com/javatest/StudyTest.java
@@ -7,6 +7,8 @@ import java.util.function.Supplier;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 //언더스코어 공백 치환
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -16,12 +18,38 @@ class StudyTest {
     @DisplayName("스터디 만들기")
     void create_new_study(){
 
-
         // assertThat(assertj 사용)
         Study actual = new Study(10);
         assertThat(actual.getLimit()).isGreaterThan(0);
 
 
+    }
+
+    @Test
+    @DisplayName("assumeTrue")
+    void assumeTrueTest(){
+        //assumeTrue : 뒤에 테스트가 실행되지 않는다.
+        String test_env = "LOCAL";
+        assumeTrue("LOCAL".equalsIgnoreCase(test_env));
+
+        // assertThat(assertj 사용)
+        Study actual = new Study(10);
+        assertThat(actual.getLimit()).isGreaterThan(0);
+    }
+
+    @Test
+    @DisplayName("assumeTrue")
+    void assumingThatTest(){
+        String test_env = "LOCAL";
+
+        //assumingThat : 앞에 조건이 만족하면 실행 로직 사용
+        assumingThat("HGS".equalsIgnoreCase(test_env),()->{
+            // 실행 로직
+        });
+
+        assumingThat("HGS".equalsIgnoreCase(test_env),()->{
+            // 실행 로직
+        });
     }
 
     @Test
