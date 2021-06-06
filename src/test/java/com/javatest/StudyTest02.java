deb7df839dfe1548d8d5cac0c3d4cd45f7e52823
hgs-study <hgstudy_@naver.com>
2021-06-06 18:30:24 +0900

    @ConvertWith : 커스텀한 타입(Study)으로 변환하고 해서 파라미터를 받고 싶을 땐 SimpleArgumentConverter 상속받은 구현체를 사용한다.

--- src/test/java/com/javatest/StudyTest02.java
+++ src/test/java/com/javatest/StudyTest02.java
@@ -3,9 +3,13 @@ package com.javatest;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.junit.jupiter.api.Assumptions.assumingThat;
 
@@ -46,8 +50,27 @@ class StudyTest02 {
     @DisplayName("파라미터 테스트")
     @ParameterizedTest(name="{index} {displayName} message={0} ")
     @ValueSource(strings = {"날씨가","많이","추워지고","있네요."})
+    @EmptySource
+    @NullSource
     void parameterizedTest(String message){
         System.out.println("message = " + message);
+    }
+
+    //파라미터 테스트 (파라미터 갯수만큼 테스트)
+    @DisplayName("파라미터 테스트")
+    @ParameterizedTest(name="{index} {displayName} message={0} ")
+    @ValueSource(ints = {10,20,30})
+//    @CsvSource({"10,'자바 스터디'","20, '스프링 스터디'"})
+    void csvSource(@ConvertWith(StudyConverter.class) Study study){
+        System.out.println("study.getLimit() = " + study.getLimit());
+    }
+    
+    static class StudyConverter extends SimpleArgumentConverter{
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            assertEquals(Study.class, targetType, "Can only convert to Study");
+            return new Study(Integer.parseInt(source.toString()));
+        }
     }
 
 
